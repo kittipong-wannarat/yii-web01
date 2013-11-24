650d27858d1b3eee02551a6d71987a5226ce9dca
kittipong wannarat <kittipong.wannarat@gmail.com>
2013-11-25 01:15:47 +0700

    edit menu

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -20,18 +20,29 @@
 	<div id="header">
 		<div id="logo"><?php echo CHtml::encode(Yii::app()->name); ?></div>
 	</div><!-- header -->
+	
+	
+	
+	
+	
 
-	<div id="mainmenu">
-		<?php $this->widget('zii.widgets.CMenu',array(
+	<div id="mainMbmenu">
+		<?php $this->widget('application.extensions.mbmenu.MbMenu',array(
 			'items'=>array(
-				array('label'=>'Home', 'url'=>array('/site/index')),
-				array('label'=>'About', 'url'=>array('/site/page', 'view'=>'about')),
-				array('label'=>'Contact', 'url'=>array('/site/contact')),
-				array('label'=>'Login', 'url'=>array('/site/login'), 'visible'=>Yii::app()->user->isGuest),
-				array('label'=>'Logout ('.Yii::app()->user->name.')', 'url'=>array('/site/logout'), 'visible'=>!Yii::app()->user->isGuest)
+				array('label'=>'Home','url'=>array('/site/index')),
+				array('url'=>array('/site/page'),'label'=>'About'),
+				array('url'=>array('/site/contact'),'label'=>'Contact' ),
+				
 			),
 		)); ?>
 	</div><!-- mainmenu -->
+	
+	
+	
+	
+	
+	
+	
 	<?php if(isset($this->breadcrumbs)):?>
 		<?php $this->widget('zii.widgets.CBreadcrumbs', array(
 			'links'=>$this->breadcrumbs,
