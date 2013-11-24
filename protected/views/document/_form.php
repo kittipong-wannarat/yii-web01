07ea0f18da58e3246fdb9185f9b00c2fe5028ffa
kittipong wannarat <kittipong.wannarat@gmail.com>
2013-11-24 10:02:48 +0700

    upload file

---
+++ protected/views/document/_form.php
@@ -0,0 +1,43 @@
+<?php
+/* @var $this DocumentController */
+/* @var $model Document */
+/* @var $form CActiveForm */
+?>
+
+<div class="form">
+
+<?php $form = $this->beginWidget('CActiveForm', array(
+	'id'=>'document-form', 
+	'enableAjaxValidation'=>false,
+	'htmlOptions' => array('enctype' => 'multipart/form-data'),
+)); ?>
+
+	<p class="note">Fields with <span class="required">*</span> are required.</p>
+
+	<?php echo $form->errorSummary($model); ?>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'id'); ?>
+		<?php echo $form->textField($model,'id'); ?>
+		<?php echo $form->error($model,'id'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'name'); ?>
+		<?php echo $form->textField($model,'name',array('size'=>60,'maxlength'=>100)); ?>
+		<?php echo $form->error($model,'name'); ?>
+	</div>
+
+	<div class="row">
+		<?php echo $form->labelEx($model,'files'); ?>
+		<?php echo $form->fileField($model,'files'); ?>
+		<?php echo $form->error($model,'files'); ?>
+	</div>
+
+	<div class="row buttons">
+		<?php echo CHtml::submitButton($model->isNewRecord ? 'Create' : 'Save'); ?>
+	</div>
+
+<?php $this->endWidget(); ?>
+
+</div><!-- form -->
---
+++ protected/views/document/_view.php
@@ -0,0 +1,21 @@
+<?php
+/* @var $this DocumentController */
+/* @var $data Document */
+?>
+
+<div class="view">
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('id')); ?>:</b>
+	<?php echo CHtml::link(CHtml::encode($data->id), array('view', 'id'=>$data->id)); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('name')); ?>:</b>
+	<?php echo CHtml::encode($data->name); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('files')); ?>:</b>
+	<?php echo CHtml::encode($data->files); ?>
+	<br />
+
+
+</div>
